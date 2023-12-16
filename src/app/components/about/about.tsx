46ee35ef2af478611b97097f620e9de1ac961f1c
Linh Hong <linh.hong.sg@gmail.com>
2023-12-16 18:10:27 +0700

    Use &#39; for ' character

--- src/app/components/about/about.tsx
+++ src/app/components/about/about.tsx
@@ -4,7 +4,7 @@ export default function About() {
         <div className="aboutSection">
             <h2>About the App</h2>
             <p>This is the 1st step in my journey to fulfill the dream of developing cool & useful features.</p>
-            <p><b>OnePage</b> is a Single Web Page Application which includes all entries to user's online resources.</p>
+            <p><b>OnePage</b> is a Single Web Page Application which includes all entries to user&#39;s online resources.</p>
             <p>This will help user to save time and get to their daily work ASAP. And it is totally free.</p>
             <p>Hope you all enjoy using it !!!</p>
             <p>All feedback are welcome! Please contact me at <a href="mailto:lilogia@gmail.com">lilogia</a>.</p>
@@ -12,7 +12,7 @@ export default function About() {
             <h3>Releases</h3>
             <h4>Delta 0.5.0 - 2023.November</h4>
             <ul>
-                <li>Browse Resources (Links and Search with Simple & RegExp) with links' logo.</li>
+                <li>Browse Resources (Links and Search with Simple & RegExp) with links&#39; logo.</li>
                 <li>CRUD for Resources in Categories, Sub Categories, Utils .</li>
                 <li>Responsive for Mobile & Desktop view.</li>
                 <li>Import & Export data (resources) for Local Storage in Browser.</li>
