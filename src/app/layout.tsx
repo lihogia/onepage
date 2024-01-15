1d6a22d4e281cde44fb944ae3c0374c7056abcec
Linh Hong <linh.hong.sg@gmail.com>
2024-01-15 15:42:13 +0700

    add Vercel Analytics to the project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import Header from '@/app/components/nav/Header';
 import '@/app/components/css/globals.css';
 import '@/app/components/css/styles.css';
 import '@/app/components/css/styles_mobile.css';
 import '@/app/components/css/styles_tablet.css';
 import '@/app/components/css/styles_desktop.css';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -24,6 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className} suppressHydrationWarning>
         {children}
+        <Analytics />
       </body>
     </html>
   )
