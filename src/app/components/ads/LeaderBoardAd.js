df3c6a0909f7197868e238766533471896f3cc99
Linh Hong <linh.hong.sg@gmail.com>
2024-01-17 15:51:38 +0700

    Fix Ads script of google, apply useEffect in LeaderBoardAd

--- src/app/components/ads/LeaderBoardAd.js
+++ src/app/components/ads/LeaderBoardAd.js
@@ -1,6 +1,12 @@
 import Script from 'next/script';
+import { useEffect } from 'react';
 
 export default function LeaderBoardAd() {
+
+    useEffect(() => {
+        let adsbygoogle = window.adsbygoogle || [];
+        (adsbygoogle).push({});
+    });
     return (
         <div className="grid2">
             <Script async src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1714100900353848' 
@@ -10,10 +16,6 @@ export default function LeaderBoardAd() {
                 data-ad-slot="7434221550"
                 data-ad-format="auto"
                 data-full-width-responsive="true"></ins>
-            <script>
-                (adsbygoogle = window.adsbygoogle || []).push({});
-            </script>
-
         </div>
     );
 }
