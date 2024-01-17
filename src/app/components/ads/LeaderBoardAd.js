4be420a49890a786a8bed59b239695e689dbe7e3
Linh Hong <linh.hong.sg@gmail.com>
2024-01-17 14:54:16 +0700

    Try to insert Google Ads

---
+++ src/app/components/ads/LeaderBoardAd.js
@@ -0,0 +1,15 @@
+export default function LeaderBoardAd() {
+    return (
+        <div className="grid2">
+            <ins className="adsbygoogle"
+                data-ad-client="ca-pub-1714100900353848"
+                data-ad-slot="7434221550"
+                data-ad-format="auto"
+                data-full-width-responsive="true"></ins>
+            <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+
+        </div>
+    );
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
-import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
 import '@/app/components/css/globals.css';
 import '@/app/components/css/styles.css';
 import '@/app/components/css/styles_mobile.css';
@@ -9,19 +9,26 @@ import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: `${process.env.productname}`,
-  description: `${process.env.tag}_${process.env.version}`
+  description: `${process.env.tag}_${process.env.version}`,
+  googleAdsenseAccount: 'ca-pub-1714100900353848'
 }
-
+/*
 export default function RootLayout({
     children,
 }: {
   children: React.ReactNode
 }) {
+*/
+export default function RootLayout({
+  children,
+}) {
 
   return (
     <html lang="en">
+        <Script async src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1714100900353848' 
+      crossorigin="anonymous"/>
       <body className={inter.className} suppressHydrationWarning>
         {children}
         <Analytics />
