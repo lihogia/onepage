a85915abab202be05f2f8da7dc60210f2e5a400b
Linh Hong <linh.hong.sg@gmail.com>
2024-04-13 15:56:17 +0700

    Fix SEO Problem in Google report for indexing: Duplicate without user-selected canonical ; Adjust layout of Donate page

--- src/app/components/donate/donate.tsx
+++ src/app/components/donate/donate.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Donate() {
+    const [qrSelect, setQRSelect] = useState(0);
+
     return (
         <div className="donateSection">
             <h2>Donation</h2>
@@ -9,11 +12,20 @@ export default function Donate() {
                 <li> 
                     <a href='https://www.buymeacoffee.com/lilogia' target='_blank'>Buy me a coffee</a>
                 </li>
-                <li>Donate a drink just by scan QR code:
+                <li>Donate a drink just by scan QR code to transfer money (Vietnam payment only):
                     <ul className="qrSection">
-                        <li>A cup of coffee - 10,000 VND<br/><br/><Image src='/imgs/QR_10K.jpg' alt='QR Code 10,000 VND' width={200} height={200} /></li>
-                        <li>A cup of milk tea - 30,000 VND<br/><br/><Image src='/imgs/QR_30K.jpg' alt='QR Code 30,000 VND' width={200} height={200} /></li>
-                        <li>A glass of fruit juice - 50,000 VND<br/><br/><Image src='/imgs/QR_50K.jpg' alt='QR Code 50,000 VND' width={200} height={200} /></li>
+                        <li><select className="inputTextLong" onChange={(e) => {
+                            setQRSelect(Number.parseInt(e.target.value));
+                        }}>
+                            <option value={0}>A cup of coffee - 10K VND</option>
+                            <option value={1}>A cup of milk tea - 30K VND</option>
+                            <option value={2}>A glass of fruit juice - 50K VND</option>
+                        </select>
+                        <br/>
+                        {qrSelect == 0 && <Image src='/imgs/QR_10K.jpg' alt='QR Code 10,000 VND' width={200} height={200} />}
+                        {qrSelect == 1 && <Image src='/imgs/QR_30K.jpg' alt='QR Code 30,000 VND' width={200} height={200} />}
+                        {qrSelect ==2 && <Image src='/imgs/QR_50K.jpg' alt='QR Code 50,000 VND' width={200} height={200} />}
+                        </li>
                     </ul>
                 </li>
             </ul>
